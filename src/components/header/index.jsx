e40984cc604596ac9328f88d51da720ef4587ce0
mortis133 <kryst.madry@gmail.com>
2023-10-15 12:36:48 +0200

    Add Routing

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import Button from '@mui/material/Button';
 import _ from 'lodash';
 import '../../MuiClassNameSetup';
 
 const Header = () => { 
   return (
     <footer>
-      <Button variant="contained">Klik - example-remote</Button>   
-      Remote - header
       <p>
-        Lodash v - {_.VERSION}
+        Remote - header - Lodash v - {_.VERSION}
       </p>
     </footer>
   );
